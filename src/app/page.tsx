c2c3e26abcc284bd8cad5734a59c2f029bdf75fe
LaCapitainerie <hugo.antreassian@gmail.com>
2024-05-14 00:09:36 +0200

    Setup Signup Form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,18 @@ import { Button } from "@/components/ui/button";
 import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+// {
+//   "type": "provider",
+//   "mail": "thomas.poupard@outlook.com",
+//   "password": "ElThomas123!",
+//   "firstName": "Thomas",
+//   "lastName": "Poupard",
+//   "phoneNumber": "0623456789"
+// }
+
 
 function Signup(){
   return (
@@ -16,14 +27,35 @@ function Signup(){
       <CardContent>
         <form>
           <div className="grid w-full items-center gap-4">
+            <div className="flex flex-row justify-between gap-4">
+              <div className="flex flex-col space-y-1.5 w-1/2">
+                <Label htmlFor="firstName">Firstname</Label>
+                <Input type="text" name="firstName" placeholder="Prénom" contrains="text"/>
+              </div>
+              <div className="flex flex-col space-y-1.5 w-1/2">
+                <Label htmlFor="lastName">Lastname</Label>
+                <Input type="text" name="lastName" placeholder="Nom" contrains="text"/>
+              </div>
+            </div>
             <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="email">Email</Label>
-              <Input type="email" name="email" placeholder="Email"/>
+              <Label htmlFor="mail">Email</Label>
+              <Input type="email" name="mail" placeholder="Email" contrains="email"/>
             </div>
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="password">Password</Label>
               <Input type="password" name="password" placeholder="Password"/>
             </div>
+            <Select>
+              <SelectTrigger id="type">
+                <SelectValue placeholder="Type" />
+              </SelectTrigger>
+              <SelectContent position="popper">
+                <SelectItem value="traveler">Traveler</SelectItem>
+                <SelectItem value="lessor">Lessor</SelectItem>
+                <SelectItem value="provider">Provider</SelectItem>
+                <SelectItem value="admin">Admin</SelectItem>
+              </SelectContent>
+            </Select>
           </div>
         </form>
       </CardContent>
