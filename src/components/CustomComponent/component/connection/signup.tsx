dc90cf8a124b92470aee79414343c721c4b52142
LaCapitainerie <hugo.antreassian@gmail.com>
2024-05-15 12:37:05 +0200

    Tweak some errors

--- src/components/CustomComponent/component/connection/signup.tsx
+++ src/components/CustomComponent/component/connection/signup.tsx
@@ -162,12 +162,6 @@ export default function Signup() {
                 <div className="grid gap-2">
                   <div className="flex items-center">
                     <Label htmlFor="password">Password</Label>
-                    <Link
-                      href="#"
-                      className="ml-auto inline-block text-sm underline"
-                    >
-                      Forgot your password?
-                    </Link>
                   </div>
                   <FormField
                     control={form.control}
@@ -260,15 +254,6 @@ export default function Signup() {
                 <Button type="submit" className="w-full">
                   Login
                 </Button>
-                <Button variant="outline" className="w-full">
-                  Login with Google
-                </Button>
-              </div>
-              <div className="mt-4 text-center text-sm">
-                Don&apos;t have an account?{" "}
-                <Link href="#" className="underline">
-                  Sign up
-                </Link>
               </div>
             </div>
           </div>
