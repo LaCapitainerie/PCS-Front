eecf223337c286568cade682a3fcc9b918503bd2
LaCapitainerie <hugo.antreassian@gmail.com>
2024-06-19 16:11:43 +0200

    Added Skeleton On Each Pages

---
+++ src/app/Admin/Dashboard/loading.tsx
@@ -0,0 +1,8 @@
+import Loading_Dashboard_Layout from "@/components/CustomComponent/loading_layout/DashboardLayout";
+
+export default async function Bailleur() {
+    return (
+        <Loading_Dashboard_Layout>
+        </Loading_Dashboard_Layout>
+    );
+}
